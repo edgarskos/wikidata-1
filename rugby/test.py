cad584736e1f9cf2e3588cdbe5b0cbee250af5f9
symac <wikitipiac@laposte.net>
2014-12-13 19:36:10 +0100

    Some cleaning

--- rugby/test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/python
-import pywikibot
-site = pywikibot.Site("fr", "wikipedia")
-
-tpl_name = "Template:Lions rugby"
-tpl_page=pywikibot.Page(site, tpl_name)
-text = tpl_page.get(get_redirect=True)
-print text
